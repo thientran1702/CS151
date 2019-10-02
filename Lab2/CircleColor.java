89a802a3b18c9f91a07e703fbbf2fcae46705daf
Thien Tran <thientran1702@gmail.com>
2019-10-02 12:54:17 -0700

    Lab 2
    Lab 2

---
+++ Lab2/CircleColor.java
@@ -0,0 +1,72 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class CircleColor 
+{
+
+        JButton greenButton, redButton, blueButton;
+        CircleFrame paintGUI;
+
+        ActionListener actionListener = new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                        if (e.getSource() == greenButton) {
+                                paintGUI.green();
+                        } else if (e.getSource() == redButton) {
+                                paintGUI.red();
+                        } else if (e.getSource() == blueButton) {
+                                paintGUI.blue();
+                        }
+                }
+        };
+
+        public static void main(String[] args) 
+        {
+                new CircleColor().show();
+        }
+
+        public void show() 
+        {
+                // create main frame
+                JFrame frame = new JFrame("Color Changer");
+                Container content = frame.getContentPane();
+
+                // set layout on content pane
+                content.setLayout(new BorderLayout());
+
+                // create draw area
+                paintGUI = new CircleFrame();
+
+                // add to content pane
+                content.add(paintGUI, BorderLayout.CENTER);
+
+                // create controls to apply colors and call clear feature
+                JPanel controls = new JPanel();
+                greenButton = new JButton("Repaint Green");
+                greenButton.addActionListener(actionListener);
+                redButton = new JButton("Repaint Red");
+                redButton.addActionListener(actionListener);
+                blueButton = new JButton("Repaint Blue");
+                blueButton.addActionListener(actionListener);
+
+                controls.add(greenButton);
+                controls.add(redButton);
+                controls.add(blueButton);
+
+                // add to content pane
+                content.add(controls, BorderLayout.SOUTH);
+
+                frame.setSize(500, 550);
+                
+                // show the swing paint result
+                frame.setVisible(true);
+
+        }
+
+}
---
+++ Lab2/CircleFrame.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.JComponent;
+
+
+public class CircleFrame extends JComponent {
+
+        // Image in which we're going to draw
+        private Image image;
+
+        // Graphics2D object ==> used to draw on
+        private Graphics2D g2;
+
+        public CircleFrame() {
+                setDoubleBuffered(false);
+        }
+
+        protected void paintComponent(Graphics g) {
+                if (image == null) 
+                {
+                        // image to draw null ==> we create
+                        image = createImage(getSize().width, getSize().height);
+                        g2 = (Graphics2D) image.getGraphics();
+
+                        // clear draw area
+                        g2.clearRect(0, 0, getSize().width, getSize().height);
+
+                        red();
+                }
+                g.drawImage(image, 0, 0, null);
+        }
+
+        public void red() {
+                // apply red color on g2 context
+                g2.clearRect(0, 0, getSize().width, getSize().height);
+                g2.setPaint(Color.red);
+                g2.fillArc(50, 50, 400, 400, 0, 360);
+                repaint();
+        }
+
+        public void green() {
+                g2.clearRect(0, 0, getSize().width, getSize().height);
+                g2.setPaint(Color.green);
+                g2.fillArc(50, 50, 400, 400, 0, 360);
+                repaint();
+        }
+
+        public void blue() {
+                g2.clearRect(0, 0, getSize().width, getSize().height);
+                g2.setPaint(Color.blue);
+                g2.fillArc(50, 50, 400, 400, 0, 360);
+                repaint();
+        }
+
+}
